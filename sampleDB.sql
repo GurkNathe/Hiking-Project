d68b62426d7367447ce67ef20e5c4f427af67de2
trisolaris3 <sj191999@gmail.com>
2021-04-20 13:45:03 -0700

    Add files via upload

--- sampleDB.sql
+++ sampleDB.sql
@@ -9,7 +9,11 @@ CREATE TABLE hikes (
 
 INSERT INTO hikes VALUES 
 (1, 'Gothic Basin', 12, 2800),
-(2, 'Oyster Dome', 8, 2000);
+(2, 'Oyster Dome', 8, 2000),
+(3, 'Camp Long', 1.6, 240),
+(4, 'Schmitz Preserve Park', 1.7, 220),
+(5, 'Lincoln Park', 1.85, 160),
+(6, 'Westcrest Park', 2.0, 220);
 
 SELECT k.*
 FROM hikes as k
